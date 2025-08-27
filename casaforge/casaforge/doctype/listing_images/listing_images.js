3683f3d931f88bfcc3d2345fedca187ae0da6b2e
ibz11 <ibramiabdi.ke@gmail.com>
2025-08-27 23:04:16 +0300

    Added the code

--- casaforge/casaforge/doctype/listing_images/listing_images.js
+++ casaforge/casaforge/doctype/listing_images/listing_images.js
@@ -1,7 +1,7 @@
 frappe.ui.form.on('Listing_Images', {
   attached_image: function (frm, cdt, cdn) {
     const row = locals[cdt][cdn];
-    // Set the preview_image to match the attached image path
+    // Set the preview_image to match the attached image
     row.preview_image = row.attached_image;
     frm.refresh_field('Images'); // Refresh the whole child table
   }
